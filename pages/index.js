504e3ed0510ae7a14dcb1b6ec83745f2b8ea8f8a
heptodrift <prendiville@gmail.com>
2026-01-07 09:58:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const FLRTracker = dynamic(() => import('../src/FLRTrackerV4'), { ssr: false });
+const FractalTerminal = dynamic(() => import('../src/FractalTerminal'), { ssr: false });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>FLR Tracker | Institutional Liquidity Analysis</title>
+        <title>Fractal Terminal | Institutional Liquidity Analysis</title>
+        <meta name="description" content="Real-time liquidity regime tracking with GEX, DIX, credit stress, and LPPL bubble detection." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <FLRTracker />
+      <FractalTerminal />
     </>
   );
 }
